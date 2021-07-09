e0d4b3a3e4fe97d48a266db42bd3e9670fabdd51
MattMuelot <mattmuelot@gmail.com>
2021-07-09 11:55:56 -0700

    New audio music and level two init

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import random
 pygame.init()
 pygame.font.init()
 game_font = pygame.font.SysFont('timesnewroman', 35)
+level_complete_font = pygame.font.SysFont('timesnewroman', 70)
 clock = pygame.time.Clock()
 width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
 bg = pygame.image.load('Assets/bground.png').convert_alpha()
 startbg = pygame.image.load('Assets/startbg.png').convert_alpha()
+level2bg = pygame.image.load('Assets/level2bg.png').convert_alpha()
 pygame.mixer.init(size=-16, channels=2)
 pygame.mixer.set_num_channels(16)
 pike_sound = pygame.mixer.Sound('Assets/pike.ogg')
 lose_sound = pygame.mixer.Sound('Assets/ugh.ogg')
-pygame.mixer.music.load('Assets/gametheme.mp3')
-pygame.mixer.music.play(-1)
 
 
 class Player:
@@ -98,6 +98,8 @@ class Bean:
 
 
 def main_menu():
+    pygame.mixer.music.load('Assets/startnoise.mp3')
+    pygame.mixer.music.play(-1)
     running = True
     while running:
         clock.tick(60)
@@ -107,16 +109,80 @@ def main_menu():
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    result = game_loop()
+                    pygame.mixer.music.stop()
+                    result = level_one()
                     if result is False:
                         running = False
+                    elif result:
+                        result = level_two()
+                        if result is False:
+                            running = False
         pygame.display.update()
 
 
-def game_loop():
+def level_two():
+    p = Player(200, 200)
+    enemies = [Enemy() for _ in range(30)]
+    beans = [Bean() for _ in range(20)]
+    running = True
+    while running:
+        clock.tick(60)
+        score_label = game_font.render(f'Score: {p.score}', True, (0, 0, 0))
+        lives_label = game_font.render(f'Lives: {p.lives}', True, (0, 0, 0))
+        screen.blit(level2bg, (0, 0))
+        screen.blit(score_label, (10, 10))
+        screen.blit(lives_label, (660, 10))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+        p.draw_player(screen)
+        p.move_player()
+        p.update_rect()
+        for b in beans[:]:
+            b.vel = 5
+            b.draw_enemy(screen)
+            b.move_enemy()
+            b.update_rect()
+            result = b.off_screen()
+            if result:
+                beans.remove(b)
+            if p.rect.colliderect(b.rect):
+                beans.remove(b)
+                pike_sound.play()
+                p.score += 1
+            if len(beans) <= 0:
+                beans = [Bean() for _ in range(20)]
+        for e in enemies[:]:
+            e.vel = 5
+            e.draw_enemy(screen)
+            e.move_enemy()
+            e.update_rect()
+            result = e.off_screen()
+            if result:
+                enemies.remove(e)
+            if p.rect.colliderect(e.rect):
+                p.lives -= 1
+                lose_sound.play()
+                enemies.remove(e)
+                if p.lives <= 0:
+                    pygame.time.delay(3000)
+                    running = False
+        if p.score >= 50:
+            pygame.time.delay(3000)
+            return True
+        else:
+            pass
+            if len(enemies) <= 0:
+                enemies = [Enemy() for _ in range(30)]
+        pygame.display.update()
+
+
+def level_one():
     p = Player(200, 200)
     enemies = [Enemy() for _ in range(20)]
     beans = [Bean() for _ in range(20)]
+    pygame.mixer.music.load('Assets/gametheme.mp3')
+    pygame.mixer.music.play(-1)
     running = True
     while running:
         clock.tick(60)
@@ -158,6 +224,15 @@ def game_loop():
                 if p.lives <= 0:
                     pygame.time.delay(3000)
                     running = False
+        if p.score >= 25:
+            level_label = level_complete_font.render('Level One Complete', True, (0, 0, 0))
+            screen.blit(level_label, (125, 350))
+            screen.blit(score_label, (10, 10))
+            pygame.display.update()
+            pygame.time.delay(3000)
+            return True
+        else:
+            pass
             if len(enemies) <= 0:
                 enemies = [Enemy() for _ in range(20)]
         pygame.display.update()
